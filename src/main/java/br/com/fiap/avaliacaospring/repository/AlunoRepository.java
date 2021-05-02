b243a96ae06f3cedb2f71053af323b445e2ac3f9
Yuri Sperandio <yuri.sperandio@gmail.com>
2021-05-02 19:51:18 -0300

    Implementando classes de teste

--- src/main/java/br/com/fiap/avaliacaospring/repository/AlunoRepository.java
+++ src/main/java/br/com/fiap/avaliacaospring/repository/AlunoRepository.java
@@ -2,6 +2,7 @@ package br.com.fiap.avaliacaospring.repository;
 
 import java.util.Optional;
 
+import br.com.fiap.avaliacaospring.model.AlunoModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import br.com.fiap.avaliacaospring.domain.AlunoDomain;
@@ -9,4 +10,6 @@ import br.com.fiap.avaliacaospring.domain.AlunoDomain;
 public interface AlunoRepository extends MongoRepository<AlunoDomain, String> {
     
     public Optional<AlunoDomain> findByRa(long ra);
+
+
 }
